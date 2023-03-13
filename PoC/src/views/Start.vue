6a6f2793aa440afbf7cb923b34701e4ba4c00a1a
roydebie <48086834+roydbie@users.noreply.github.com>
2023-03-13 20:28:23 +0100

    Axios test met public API

--- PoC/src/views/Start.vue
+++ PoC/src/views/Start.vue
@@ -5,5 +5,24 @@
 <template>
   <main>
     Start  hallooooo
+    {{ data }}
   </main>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+      return {
+        data: null
+      }
+    },
+  mounted() {
+    axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(response => (this.data = response))
+  }
+
+}
+</script>
