f5bb62b39e89563e6fcc1df4db4f9fc240342971
roydebie <48086834+roydbie@users.noreply.github.com>
2023-03-14 20:11:14 +0100

    Lokaal json bestand gebruikt in plaats van API

--- PoC/src/views/Local.vue
+++ PoC/src/views/Local.vue
@@ -4,6 +4,69 @@
 
 <template>
   <main>
-    Settings ja toch
+
+    <div v-if="loading === true" class="m-auto" style="text-align: center;">
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+
+    <div v-if="loading === false">
+
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Stadsdeel</th>
+            <th scope="col">Buurt</th>
+            <th scope="col">Aantal afvalbakken</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="row in afvalbakkenData">
+            <template v-if="row[0] && row[1]">
+              <th scope="row">{{ row[0] }}</th>
+              <td>{{ row[1] }}</td>
+              <td>{{ row[2] }}</td>
+            </template>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
   </main>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+      return {
+        afvalbakkenData: null,
+        loading: false
+      }
+    },
+  mounted() {
+    this.loading = true;
+    axios
+        .get('afvalbakken.json')
+        .then(response => {
+
+          let uniekeElementen = [...new Set(response.data)];
+
+          const elementenTellen = uniekeElementen.map(value => [value.stadsdeel, value.buurt, response.data.filter(str => str.buurt === value.buurt && str.stadsdeel === value.stadsdeel).length]);
+
+          this.afvalbakkenData = elementenTellen.filter((value, index) => {
+            const _value = JSON.stringify(value);
+            return index === elementenTellen.findIndex(obj => {
+              return JSON.stringify(obj) === _value;
+            });
+          }).sort();
+        })
+        .then(() => {
+          this.loading = false
+        })
+  }
+
+}
+</script>
