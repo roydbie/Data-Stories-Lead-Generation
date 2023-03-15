c40c5e7d3c0c832b8faa7abbe1a8e0ee648595d4
roydebie <48086834+roydbie@users.noreply.github.com>
2023-03-15 19:36:21 +0100

    Grafiek aangemaakt en data komt uit het JSON bestand

--- PoC/src/components/WasteBinsChart.vue
+++ PoC/src/components/WasteBinsChart.vue
@@ -1,25 +1,59 @@
 <template>
-  <div id="WasteBinsChart"></div>
+  <Scatter
+      :options="chartOptions"
+      :data="chartData"
+      v-if="loaded"
+  />
 </template>
 
 <script>
+import {
+  Chart as ChartJS,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Scatter } from 'vue-chartjs'
+
+ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend)
 
 export default {
   name: "WasteBinsChart",
+  components: { Scatter },
   props: {
     data: Array,
   },
   data() {
     return {
-      chartData: [],
+      chartData: {
+        datasets: [
+          {
+            label: 'Test voor nu',
+            fill: false,
+            borderColor: '#f87979',
+            backgroundColor: '#f87979',
+            data: []
+          }
+        ]
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      },
+      loaded: false
     }
   },
   mounted() {
+    const datasets = [];
     this.data.forEach(item => {
       if (item.amountOfBins !== null && item.amountOfResidents !== null) {
-        this.chartData.push({amountOfBins: item.amountOfBins, amountOfResidents: item.amountOfResidents})
+        datasets.push({x: item.amountOfResidents, y: item.amountOfBins});
       }
     })
+    this.chartData.datasets[0].data = datasets;
+    this.loaded = true;
   },
 };
 
