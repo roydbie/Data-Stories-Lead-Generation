5b46cdde10221b4acc2ad39cae1226c108d0e923
roydebie <48086834+roydbie@users.noreply.github.com>
2023-03-18 16:58:03 +0100

    Admin dashboard - video tot 53:37

--- Python POC/python/main/views.py
+++ Python POC/python/main/views.py
@@ -4,6 +4,10 @@ from .models import Neighbourhood
 
 # Create your views here.
 
-def index(response, id):
+def index(response):
+    return HttpResponse("<h1>Jooo index</h1>")
+
+def neighbourhood(response, id):
     x = Neighbourhood.objects.get(id=id)
     return HttpResponse("<h1>%s<h1>" % x.name)
+
