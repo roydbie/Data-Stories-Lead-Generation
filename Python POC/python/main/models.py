f019df6e477c4304178c2eda9eadd107d8fae4aa
roydebie <48086834+roydbie@users.noreply.github.com>
2023-03-18 11:40:42 +0100

    Basis database dingen - video tot 44:33

--- Python POC/python/main/models.py
+++ Python POC/python/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.IntegerField()
+    bins = models.IntegerField()
+    residents = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- Python POC/python/main/views.py
+++ Python POC/python/main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Neighbourhood
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>joooo!<h1>")
+def index(response, id):
+    x = Neighbourhood.objects.get(id=id)
+    return HttpResponse("<h1>%s<h1>" % x.name)
