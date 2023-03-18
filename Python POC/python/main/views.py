d92e64618529881ac9e89074f7691350f7c4a7b2
roydebie <48086834+roydbie@users.noreply.github.com>
2023-03-18 20:41:39 +0100

    API data verkregen

--- Python POC/python/main/views.py
+++ Python POC/python/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Neighbourhood
+import requests
 
 # Create your views here.
 
@@ -8,8 +9,9 @@ def home(response):
     return render(response, "main/home.html", {})
 
 def neighbourhoods(response):
-    x = Neighbourhood.objects.all()
-    return render(response, "main/neighbourhoods.html", {"neighbourhoods": x})
+    nbhs = requests.get('https://data.eindhoven.nl/api/records/1.0/search/?dataset=afvalbakken0&q=&rows=10000&sort=buurt&facet=stadsdeel&facet=buurt&facet=straat&facet=eigenaar&facet=beheerder&facet=soort&facet=voet&facet=kleur&facet=locatie&facet=inhoud')
+    data = nbhs.json()
+    return render(response, "main/neighbourhoods.html", {"data": data})
 
 def neighbourhood(response, id):
     x = Neighbourhood.objects.get(id=id)
