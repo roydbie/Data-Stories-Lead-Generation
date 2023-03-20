fd8c8d1a0df89de901f2b826f73b02353a1d5365
roydebie <48086834+roydbie@users.noreply.github.com>
2023-03-20 12:10:16 +0100

    API data naar tabel gezet (helaas een maximum van 100)

--- Python POC/python/main/views.py
+++ Python POC/python/main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Neighbourhood
 import requests
+from collections import Counter
+
 
 # Create your views here.
 
@@ -9,9 +11,18 @@ def home(response):
     return render(response, "main/home.html", {})
 
 def neighbourhoods(response):
-    nbhs = requests.get('https://data.eindhoven.nl/api/records/1.0/search/?dataset=afvalbakken0&q=&rows=10000&sort=buurt&facet=stadsdeel&facet=buurt&facet=straat&facet=eigenaar&facet=beheerder&facet=soort&facet=voet&facet=kleur&facet=locatie&facet=inhoud')
-    data = nbhs.json()
-    return render(response, "main/neighbourhoods.html", {"data": data})
+    request = requests.get('https://data.eindhoven.nl/api/records/1.0/search/?dataset=afvalbakken0&q=&rows=10000&facet=buurt')
+    data = request.json()
+
+    facets = data['facet_groups'][0]['facets']
+    facet_values = []
+    for facet in facets:
+        facet_values.append({
+            "name": facet['name'],
+            "count": facet['count'],
+        })
+
+    return render(response, "main/neighbourhoods.html", {"data": facet_values, "length": len(facet_values)})
 
 def neighbourhood(response, id):
     x = Neighbourhood.objects.get(id=id)
