2a00b358442cd0e79aab5058c55114348b58190f
roydebie <48086834+roydbie@users.noreply.github.com>
2023-03-20 13:38:21 +0100

    Afbalbakkendata gecombineerd met buurten

--- Python POC/python/main/views.py
+++ Python POC/python/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Neighbourhood
 import requests
 from collections import Counter
@@ -11,18 +11,33 @@ def home(response):
     return render(response, "main/home.html", {})
 
 def neighbourhoods(response):
-    request = requests.get('https://data.eindhoven.nl/api/records/1.0/search/?dataset=afvalbakken0&q=&rows=10000&facet=buurt')
-    data = request.json()
+    neighbourhoods = requests.get('https://data.eindhoven.nl/api/records/1.0/search/?dataset=buurten&q=&fields=buurtcode,buurtnaam&rows=10000')
+    neighbourhoods_data = neighbourhoods.json()
 
-    facets = data['facet_groups'][0]['facets']
-    facet_values = []
-    for facet in facets:
-        facet_values.append({
-            "name": facet['name'],
-            "count": facet['count'],
-        })
+    neighbourhood_records = neighbourhoods_data['records']
+    main_data_array = []
+    for record in neighbourhood_records:
+        main_data_array.append({"name": record['fields'].get('buurtnaam'), "code": record['fields'].get('buurtcode'), "waste_bins": 0, "residents": 0})
 
-    return render(response, "main/neighbourhoods.html", {"data": facet_values, "length": len(facet_values)})
+    waste_bins = requests.get('https://data.eindhoven.nl/api/records/1.0/search/?dataset=afvalbakken0&q=&rows=10000&fields=buurt')
+    waste_bins_data = waste_bins.json()
+
+    counter = Counter(neighbourhood['fields'].get('buurt') for neighbourhood in waste_bins_data['records'])
+
+    waste_bins_array = []
+    for name, waste_bins in counter.items():
+        waste_bins_array.append({"name": name, "waste_bins": waste_bins})
+
+    # First, create a dictionary from the second list for quick lookup
+    waste_bins_dict = {item['name']: item for item in waste_bins_array}
+
+    # Then, iterate over the first list and add the corresponding object from the second list
+    for item in main_data_array:
+        if item['name'] in waste_bins_dict:
+            second_item = waste_bins_dict[item['name']]
+            item['waste_bins'] = second_item['waste_bins']
+
+    return render(response, "main/neighbourhoods.html", {"data": main_data_array, "length": len(main_data_array)})
 
 def neighbourhood(response, id):
     x = Neighbourhood.objects.get(id=id)
