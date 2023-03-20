f326b159bfd4d6a548a8781b715bcb799bcd2e99
roydebie <48086834+roydbie@users.noreply.github.com>
2023-03-20 15:01:03 +0100

    Inwonersdata gecombineerd met buurten

--- Python POC/python/main/views.py
+++ Python POC/python/main/views.py
@@ -19,6 +19,8 @@ def neighbourhoods(response):
     for record in neighbourhood_records:
         main_data_array.append({"name": record['fields'].get('buurtnaam'), "code": record['fields'].get('buurtcode'), "waste_bins": 0, "residents": 0})
 
+
+    # Add waste bins to the array
     waste_bins = requests.get('https://data.eindhoven.nl/api/records/1.0/search/?dataset=afvalbakken0&q=&rows=10000&fields=buurt')
     waste_bins_data = waste_bins.json()
 
@@ -36,6 +38,23 @@ def neighbourhoods(response):
         if item['name'] in waste_bins_dict:
             second_item = waste_bins_dict[item['name']]
             item['waste_bins'] = second_item['waste_bins']
+
+    # Add residents to the array
+    residents = requests.get('https://data.eindhoven.nl/api/records/1.0/search/?dataset=selectiontableasjsonashx&q=&rows=10000&refine.jaar=2023&fields=buurtcode,totaal_aantal_inwoners')
+    residents_data = residents.json()
+
+    residents_array = []
+    for neighbourhood in residents_data['records']:
+        residents_array.append({"code": neighbourhood['fields'].get('buurtcode'), "residents": neighbourhood['fields'].get('totaal_aantal_inwoners')})
+
+    # First, create a dictionary from the second list for quick lookup
+    residents_dict = {item['code']: item for item in residents_array}
+
+    # Then, iterate over the first list and add the corresponding object from the second list
+    for item in main_data_array:
+        if item['code'] in residents_dict:
+            second_item = residents_dict[item['code']]
+            item['residents'] = second_item['residents']
 
     return render(response, "main/neighbourhoods.html", {"data": main_data_array, "length": len(main_data_array)})
 
